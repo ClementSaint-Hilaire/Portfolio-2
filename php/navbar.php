f85d2c3af1b8c496f782b42b3878fa5ba860e7c0
cshd <clement.sainthilaire@sts-sio-caen.info>
2024-03-28 19:41:16 +0100

    font & navbar

---
+++ php/navbar.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="../img/logo.png" />
+    <link rel="stylesheet" href="../css/navbar.css">
+    <title>csh</title>
+</head>
+<body>
+    
+</body>
+</html>
+<section class="navbar_container">
+    <div class="content_left">
+        <a href="#">HOME</a>
+        <a href="#">CREATIONS</a>
+        <a href="#">ABOUT</a>
+        <a href="#" target="_blank">CARDS</a>
+    </div>
+    <a href="#" class="content_right">CONTACT</a>
+</section>

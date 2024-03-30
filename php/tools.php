97a332880c3745246c752f0f7a74acb216ac7a76
cshd <clement.sainthilaire@sts-sio-caen.info>
2024-03-30 19:13:23 +0100

    apps cards

--- php/tools.php
+++ php/tools.php
@@ -11,7 +11,50 @@
 
 <section class="tools_container">
     <img src="../img/tools.png" alt="tools">
+
+    <div class="bouton_my_apps_container">
+        <button class="bouton_apps">
+            <h1>My apps</h1>
+            <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect x="14" width="2" height="30" fill="#75757A"/>
+                <rect y="16" width="2" height="30" transform="rotate(-90 0 16)" fill="#75757A"/>
+            </svg>
+        </button>
+        <div class="apps_container">
+            <button class="bouton_apps_close">
+                <h1>My apps</h1>
+                <svg width="30" height="3" viewBox="0 0 30 3" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect y="2.18164" width="2" height="30" transform="rotate(-90 0 2.18164)" fill="#75757A"/>
+                </svg>
+            </button>
+            <div class="app_cards_container">
+                <?php
+                    include '../config/database.php';
+                    $query = $db->prepare("SELECT * FROM appcard");
+                    if ($query->execute()) {
+                        $query->bindColumn('imglink', $img);
+                        $query->bindColumn('nom', $nom);
+                        $query->bindColumn('categorie', $categorie);
+                        $query->bindColumn('description', $description);
+                        while ($query->fetch(PDO::FETCH_BOUND)) {
+                        
+                            echo '<div class="appCard">';
+                                echo '<h1>' . htmlspecialchars($categorie) . '</h1>';
+                                echo '<h2>' . htmlspecialchars($nom) . '</h2>';
+                                echo '<img src="' . htmlspecialchars($img) . '">';
+                                echo '<h3>' . htmlspecialchars($description) . '</h3>';
+                            echo '</div>';
+                        }
+                    } else {
+                        echo "Erreur lors de l'exécution de la requête : " . $query->errorInfo()[2];
+                    }
+                    $db = null; 
+                ?>
+            </div>
+        </div>
+    </div>
 </section>
 
 </body>
+<script src="../js/apps_bouton.js"></script>
 </html>

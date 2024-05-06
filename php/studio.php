5ae86ee62cb3cbe016bdcb75fa7fe78c98663438
ClémentSaintHilaire <clement.sainthilaire.pro@gmail.com>
2024-05-06 10:39:29 +0200

    update studio page

--- php/studio.php
+++ php/studio.php
@@ -8,12 +8,14 @@
 </head>
 <body>
 
+<?php include 'navbar.php';?>
+
 <section class="comming_soon_container">
     <h1>Comming Soon</h1>
     <h2>Get notifiy by mail</h2>
 </section>
     
-<?php include 'contact.php'?>
+<?php include 'contact.php';?>
 
 </body>
 </html>
